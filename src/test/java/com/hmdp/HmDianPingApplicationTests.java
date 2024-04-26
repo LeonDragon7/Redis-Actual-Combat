20a3e8873de55425071d416259bc0b045d703f31
LeonDragon7 <209418465@qq.com>
2024-04-26 20:58:11 +0800

    feat: 解决缓存击穿-逻辑时间

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,23 @@
 package com.hmdp;
 
+import com.hmdp.service.IShopService;
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Autowired
+    private ShopServiceImpl shopService;
 
+    /**
+     * 缓存击穿-逻辑时间
+     */
+    @Test
+    public void queryWithLogicalTimeTest() throws InterruptedException {
+        shopService.saveShopRedis(1L,10L);
+    }
 }
