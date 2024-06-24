b65ebb9626bbe212d5a10f7796c645a695ea9451
SevenZT <209418465@qq.com>
2024-06-24 20:52:18 +0800

    feat: lua脚本解决多条命令原子性问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -11,6 +14,12 @@ public class SimpleRedisLock implements ILock{
     private String name;
     private final static String key_prefix = "lock:";
     private final static String id_prefix = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     private StringRedisTemplate stringRedisTemplate;
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -29,14 +38,21 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        //获取当前线程标识
-        String threadId = id_prefix + Thread.currentThread().getId();
-        //获取锁总的线程标识
-        String id = stringRedisTemplate.opsForValue().get(key_prefix + name);
-        //判断上述是否相等
-        if(threadId.equals(id)){
-            //释放锁
-            stringRedisTemplate.delete(key_prefix + name);
-        }
+
+        //方法一：
+//        //获取当前线程标识
+//        String threadId = id_prefix + Thread.currentThread().getId();
+//        //获取锁总的线程标识
+//        String id = stringRedisTemplate.opsForValue().get(key_prefix + name);
+//        //判断上述是否相等
+//        if(threadId.equals(id)){
+//            //释放锁
+//            stringRedisTemplate.delete(key_prefix + name);
+//        }
+
+        //方法二：调用lua脚本，保证判断和释放的原子性
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(key_prefix + name),
+                id_prefix + Thread.currentThread().getId());
     }
 }
